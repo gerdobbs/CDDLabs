c40290b1e8b57832852309d70a6cdd37bca768fd
ger <dobbsger0@gmail.com>
2017-10-06 15:27:34 +0100

    Variable added to Lab3

--- Lab3/signal.cpp
+++ Lab3/signal.cpp
@@ -3,34 +3,47 @@
 @date 02/10/2017
 @version 1.0
 */
+/*!
+Semaphore used to apply mutual exclusion.
+Whichever thread runs first will apply a mutex.
 
+*/
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
 
-void taskOne(std::shared_ptr<Semaphore> theSemaphore){
-  std::cout <<"I ";
-  std::cout << "must ";
-  std::cout << "print";
-  std::cout << "first"<<std::endl;
-  theSemaphore->Signal();
+int shared_variable = 0;
+
+void taskOne(std::shared_ptr<Semaphore> theMutex){
+	theMutex->Wait();
+	shared_variable++;
+	std::cout <<"Increase "<<std::endl;
+	std::cout << "shared "<<std::endl;
+	std::cout << "variable by 1 " <<std::endl;
+	std::cout << "Value is now: " ;  
+        std::cout << shared_variable<<std::endl;
+	theMutex->Signal();
 }
-void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
-  theSemaphore->Wait();
-  std::cout <<"This ";
-  std::cout << "will";
-  std::cout << "appear ";
-  std::cout << "second"<<std::endl;
+void taskTwo(std::shared_ptr<Semaphore> theMutex){
+	theMutex->Wait();
+       
+	 shared_variable= shared_variable+1;
+	std::cout <<"Increase "<<std::endl;
+	std::cout << "shared "<<std::endl;
+	std::cout << "variable by 1 " <<std::endl;
+	std::cout << "Value is now: " ;  
+        std::cout <<  shared_variable <<std::endl;
+	theMutex->Signal();
 }
 
 int main(void){
-  std::thread threadOne, threadTwo;
-  std::shared_ptr<Semaphore> sem1( new Semaphore);
-  /**< Launch the threads  */
-  threadOne=std::thread(taskTwo,sem1);
-  threadTwo=std::thread(taskOne,sem1);
-  std::cout << "Launched from the main\n";
-  threadOne.join();
-  threadTwo.join();
-  return 0;
+	std::thread threadOne, threadTwo;
+	std::shared_ptr<Semaphore> theMutex( new Semaphore(6));
+	/**< Launch the threads  */
+	threadOne=std::thread(taskTwo,theMutex);
+	threadTwo=std::thread(taskOne,theMutex);
+	std::cout << "Launched from the main\n";
+	threadOne.join();
+	threadTwo.join();
+	return 0;
 }
